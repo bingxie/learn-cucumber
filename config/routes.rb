45529a020fcf7883c07c55035a704967625a5033
Bing Xie <bing708@gmail.com>
2015-02-03 19:23:00 +1100

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ LearnCucumber::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match 'signup', :controller => 'users', :action => 'new'
+  match 'logout', :controller => 'sessions', :action => 'destroy'
+  match 'login', :controller => 'sessions', :action => 'new'
+
+  resources :users
+  resources :sessions
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +55,7 @@ LearnCucumber::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'articles#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150203044440) do
+ActiveRecord::Schema.define(:version => 20150203075027) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.boolean  "admin"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
