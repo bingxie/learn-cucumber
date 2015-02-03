0839fc59228254759ac1f67c6b730b36ce4c0b20
Bing Xie <bing708@gmail.com>
2015-02-03 15:59:52 +1100

    Add list articles feature and test

---
+++ features/step_definitions/article_steps.rb
@@ -0,0 +1,13 @@
+Given(/^I have articles titled (.+)$/) do |titles|
+  titles.split(', ').each do |title|
+    Article.create!(:title => title)
+  end
+end
+
+When(/^I go to the list of articles$/) do
+  visit '/articles'
+end
+
+Then(/^I should see "(.*?)"$/) do |title|
+  page.should have_content title
+end
