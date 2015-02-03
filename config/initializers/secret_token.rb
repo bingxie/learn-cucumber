1058cfb7243f43b9cb75399df1a3a989c6459a91
Bing Xie <bing708@gmail.com>
2015-02-03 15:01:51 +1100

    New app with cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnCucumber::Application.config.secret_token = '2f2b28fe624cc57a99696b3d18a4227495521ea90ab00450a4c5648bbbc868a5fdea01c0dfe4e2f5d81f68214fdd8c5c378237fb09238dcf830e127c48283364'
